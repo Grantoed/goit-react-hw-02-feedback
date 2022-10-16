3091a6a5c433fbea6a9dd2162cf2012f05056710
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-10-16 17:21:51 +0300

    Feedback form completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Box } from './Box';
+
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onButtonClick = e => {
+    this.setState(prevState => ({
+      [e.target.name]: prevState[e.target.name] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, statValue) => {
+      return acc + statValue;
+    }, 0);
+  };
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  }
+
+  render() {
+    const statOptions = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    return (
+      <Box width="50%" p="15px">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            statOptions={statOptions}
+            onButtonClick={this.onButtonClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total ? (
+            <Statistics
+              stats={this.state}
+              statOptions={statOptions}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <p>No feedback given</p>
+          )}
+        </Section>
+      </Box>
+    );
+  }
+}
